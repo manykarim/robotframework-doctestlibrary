8202574a4303ad7f6132e7009ebe1bb92b7e2ad8
Many Kasiriha <many.kasiriha@dbschenker.com>
2021-03-10 21:11:41 +0100

    add deepdiff dependency

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="robotframework-doctestlibrary",
-    version="0.1.1",
+    version="0.1.2",
     author="Many Kasiriha",
     author_email="many.kasiriha@dbschenker.com",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/manykarim/robotframework-doctestlibrary",
     packages=find_packages(),
-    install_requires=['imutils', 'numpy', 'opencv-python-headless', 'parsimonious', 'pytesseract', 'robotframework', 'scikit-image', 'Wand', 'pylibdmtx', 'pdfminer.six'],
+    install_requires=['imutils', 'numpy', 'opencv-python-headless', 'parsimonious', 'pytesseract', 'robotframework', 'scikit-image', 'Wand', 'pylibdmtx', 'pdfminer.six', 'deepdiff'],
     dependency_links=['https://github.com/tesseract-ocr/tesseract', 'https://www.ghostscript.com/download/gsdnld.html' , 'https://www.ghostscript.com/download/gpcldnld.html', 'https://imagemagick.org/script/download.php'],
     zip_safe=False
 )
