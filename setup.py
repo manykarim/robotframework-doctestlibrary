07ced02ae849dd79abe567524a75c428a6c0a9d0
Many Kasiriha <61293164+manykarim@users.noreply.github.com>
2021-08-19 21:08:55 +0200

    Add datetime.datetime.now().strftime("%Y%m%d%H%M%S") to version

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
+import datetime
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
     name="robotframework-doctestlibrary",
-    version="0.2.0-dev",
+    version="0.2.0.dev" + datetime.datetime.now().strftime("%Y%m%d%H%M%S")",
     author="Many Kasiriha",
     author_email="many.kasiriha@dbschenker.com",
     long_description=long_description,
