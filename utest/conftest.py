659fad07a4912900c043159f8fc95d4ac9620951
manykarim <61293164+manykarim@users.noreply.github.com>
2021-10-26 10:56:17 +0000

    Deploying to main from @ manykarim/robotframework-doctestlibrary@1ac1455fac75f791186f26df77c8780fe9186c9b 🚀

--- utest/conftest.py
+++
@@ -1,8 +0,0 @@
-import  pytest
-from pathlib import Path
-
-
-@pytest.fixture
-def testdata_dir():
-    testdata_directory = Path(__file__).parent.resolve() / "testdata"
-    return  testdata_directory
