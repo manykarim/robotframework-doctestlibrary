b6ad60a12de8555bb1567f1d56473b9841a70991
Many Kasiriha <many.kasiriha@dbschenker.com>
2022-10-05 17:47:17 +0200

    Add test to compare two URLs

--- utest/test_visualtest.py
+++ utest/test_visualtest.py
@@ -19,3 +19,10 @@ def test_get_text_from_pdf_does_not_match(testdata_dir):
     ref_image=str(testdata_dir / 'sample.pdf')
     text = visual_tester.get_text_from_document(ref_image)
     assert 'THIS STRING DOES NOT EXIST' not in text
+
+def test_compare_two_different_images_from_url():
+    visual_tester = VisualTest()
+    ref_image='https://github.com/manykarim/robotframework-doctestlibrary/raw/main/testdata/Beach_left.jpg'
+    test_image='https://github.com/manykarim/robotframework-doctestlibrary/raw/main/testdata/Beach_right.jpg'
+    with pytest.raises(AssertionError):
+        visual_tester.compare_images(ref_image, test_image)
