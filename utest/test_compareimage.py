98d51e22a537d1e7aedb20881203ee3f69701a26
Many Kasiriha <many.kasiriha@dbschenker.com>
2022-10-05 16:43:42 +0200

    Add tests for loading from URL

--- utest/test_compareimage.py
+++ utest/test_compareimage.py
@@ -16,6 +16,21 @@ def test_single_pdf(testdata_dir):
     assert type(img.opencv_images[0])==numpy.ndarray
     pass
 
+def test_image_from_url():
+    img = CompareImage('https://github.com/manykarim/robotframework-doctestlibrary/raw/main/testdata/Beach_left.jpg')
+    assert len(img.opencv_images)==1
+    assert type(img.opencv_images)==list
+    assert type(img.opencv_images[0])==numpy.ndarray
+    pass
+
+def test_multipage_pdf_from_url():
+    img = CompareImage('https://github.com/manykarim/robotframework-doctestlibrary/raw/main/testdata/sample.pdf')
+    assert len(img.opencv_images)==2
+    assert type(img.opencv_images)==list
+    assert type(img.opencv_images[0])==numpy.ndarray
+    assert type(img.opencv_images[1])==numpy.ndarray
+    pass
+
 def test_multipage_pdf(testdata_dir):
     img = CompareImage(testdata_dir / 'sample.pdf')
     assert len(img.opencv_images)==2
