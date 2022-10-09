68ee3769d8cb902dc0e991caf8c3af26bb551d08
Many Kasiriha <many.kasiriha@dbschenker.com>
2022-10-09 22:38:54 +0200

    Add tests for blur and threshold

--- utest/test_visualtest.py
+++ utest/test_visualtest.py
@@ -26,3 +26,30 @@ def test_compare_two_different_images_from_url():
     test_image='https://github.com/manykarim/robotframework-doctestlibrary/raw/main/testdata/Beach_right.jpg'
     with pytest.raises(AssertionError):
         visual_tester.compare_images(ref_image, test_image)
+
+def test_compare_birthday_image_with_noise(testdata_dir):
+    visual_tester = VisualTest()
+    ref_image=str(testdata_dir / 'birthday_1080.png')
+    cand_image=str(testdata_dir / 'birthday_1080_noise_001.png')
+    with pytest.raises(AssertionError, match='The compared images are different.'):
+        visual_tester.compare_images(ref_image, cand_image)
+
+def test_compare_birthday_image_with_different_noise(testdata_dir):
+    visual_tester = VisualTest()
+    ref_image=str(testdata_dir / 'birthday_1080_noise_001.png')
+    cand_image=str(testdata_dir / 'birthday_1080_noise_002.png')
+    with pytest.raises(AssertionError, match='The compared images are different.'):
+        visual_tester.compare_images(ref_image, cand_image)
+
+
+def test_compare_birthday_image_with_noise_and_lower_threshold(testdata_dir):
+    visual_tester = VisualTest(threshold=0.2)
+    ref_image=str(testdata_dir / 'birthday_1080.png')
+    cand_image=str(testdata_dir / 'birthday_1080_noise_001.png')
+    visual_tester.compare_images(ref_image, cand_image)
+
+def test_compare_birthday_image_with_noise_and_blurring(testdata_dir):
+    visual_tester = VisualTest(threshold=0.005)
+    ref_image=str(testdata_dir / 'birthday_1080.png')
+    cand_image=str(testdata_dir / 'birthday_1080_noise_001.png')
+    visual_tester.compare_images(ref_image, cand_image, blur=True)
