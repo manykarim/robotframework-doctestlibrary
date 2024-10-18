3e3b82426929bdbb0797cda91568cb643f11913c
Many Kasiriha <many.kasiriha@gmail.com>
2024-10-19 00:29:39 +0200

    Update tests to use DocumentRepresentation

--- utest/test_masks.py
+++ utest/test_masks.py
@@ -1,46 +1,30 @@
-from DocTest.CompareImage import CompareImage
+from DocTest.DocumentRepresentation import DocumentRepresentation
 import pytest
 from pathlib import Path
-
-def test_image_coordinate_mask(testdata_dir):
-    pass
+import numpy as np
 
 def test_image_area_mask(testdata_dir):
-    img = CompareImage(testdata_dir / 'Beach_date.png', placeholder_file=testdata_dir / 'area_mask.json')
-    assert len(img.placeholders)==1
-    img_with_mask = img.get_image_with_placeholders()
-    assert img != img_with_mask
+    img = DocumentRepresentation(testdata_dir / 'Beach_date.png', ignore_area_file=testdata_dir / 'area_mask.json')
+    assert len(img.abstract_ignore_areas)==1
+    assert np.not_equal(img.pages[0].get_image_with_ignore_areas(), img.pages[0].image).any()
+
 
 def test_image_text_mask(testdata_dir):
-    img = CompareImage(testdata_dir / 'Beach_date.png', placeholder_file=testdata_dir / 'pattern_mask.json')
-    assert len(img.placeholders)==2
-    img_with_mask = img.get_image_with_placeholders()
-    assert img != img_with_mask
+    img = DocumentRepresentation(testdata_dir / 'Beach_date.png', ignore_area_file=testdata_dir / 'pattern_mask.json')
+    assert len(img.abstract_ignore_areas)==2
+    assert np.not_equal(img.pages[0].get_image_with_ignore_areas(), img.pages[0].image).any()
 
 def test_image_text_mask_with_east(testdata_dir):
-    img = CompareImage(testdata_dir / 'Beach_date.png', placeholder_file=testdata_dir / 'pattern_mask.json', ocr_engine='east')
-    assert len(img.placeholders)>=1
-    img_with_mask = img.get_image_with_placeholders()
-    assert img != img_with_mask
-
-def test_image_barcode_mask(testdata_dir):
-    pass
-
-def test_pdf_coordinate_mask(testdata_dir):
-    pass
+    img = DocumentRepresentation(testdata_dir / 'Beach_date.png', ignore_area_file=testdata_dir / 'pattern_mask.json', ocr_engine='east')
+    assert len(img.abstract_ignore_areas)>=1
+    assert np.not_equal(img.pages[0].get_image_with_ignore_areas(), img.pages[0].image).any()
 
 def test_pdf_area_mask(testdata_dir):
-    img = CompareImage(testdata_dir / 'sample_1_page.pdf', placeholder_file=testdata_dir / 'pdf_area_mask.json')
-    assert len(img.placeholders)==1
-    img_with_mask = img.get_image_with_placeholders()
-    assert img != img_with_mask
+    img = DocumentRepresentation(testdata_dir / 'sample_1_page.pdf', ignore_area_file=testdata_dir / 'pdf_area_mask.json')
+    assert len(img.abstract_ignore_areas)==1
+    assert np.not_equal(img.pages[0].get_image_with_ignore_areas(), img.pages[0].image).any()
 
 def test_pdf_text_mask(testdata_dir):
-    img = CompareImage(testdata_dir / 'sample_1_page.pdf', placeholder_file=testdata_dir / 'pdf_pattern_mask.json')
-    assert len(img.placeholders)==3
-    img_with_mask = img.get_image_with_placeholders()
-    assert img != img_with_mask
-
-def test_pdf_barcode_mask(testdata_dir):
-    pass
-
+    img = DocumentRepresentation(testdata_dir / 'sample_1_page.pdf', ignore_area_file=testdata_dir / 'pdf_pattern_mask.json')
+    assert len(img.abstract_ignore_areas)==3
+    assert np.not_equal(img.pages[0].get_image_with_ignore_areas(), img.pages[0].image).any()
